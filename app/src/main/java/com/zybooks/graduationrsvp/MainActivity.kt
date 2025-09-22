4292731dcc4d50237457afbc1f3c8798296e36fa
Andrew <arvizumunozandrew@gmail.com>
2025-09-21 18:18:50 -0700

    Finishing button and info portion of app

--- app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -37,20 +39,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(R.drawable.grad_cap),
-        contentDescription = "Graduation cap",
-        alpha = 0.3f
-    )
-    Text(
-        text = stringResource(R.string.graduation_announcement),
-        fontSize = 50.sp,
-        fontWeight = FontWeight.Bold,
-        textAlign = TextAlign.Center,
-        color = Color.Red,
-        lineHeight = 50.sp,
-        modifier = modifier
-    )
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = stringResource(R.string.graduation_announcement),
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            color = Color.Red,
+            lineHeight = 50.sp,
+            modifier = modifier
+        )
+        Image(
+            painter = painterResource(R.drawable.grad_cap),
+            contentDescription = "Graduation cap",
+            alpha = 0.3f
+        )
+        Text(
+            text = stringResource(R.string.info)
+        )
+    }
+
+
 }
 
 @Preview(showBackground = true)

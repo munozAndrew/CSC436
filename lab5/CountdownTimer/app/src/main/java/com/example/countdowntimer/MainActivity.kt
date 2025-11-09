aae277c5403d5709f200e42a7d80a1ea30f89064
Andrew <arvizumunozandrew@gmail.com>
2025-11-08 16:25:38 -0800

    Lab4 Done

---
+++ lab5/CountdownTimer/app/src/main/java/com/example/countdowntimer/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.countdowntimer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
+import androidx.work.workDataOf
+import com.example.countdowntimer.ui.TimerScreen
+import com.example.countdowntimer.ui.TimerViewModel
+import com.example.countdowntimer.ui.theme.CountdownTimerTheme
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+
+class MainActivity : ComponentActivity() {
+
+    private val permissionRequestLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            val message = if (isGranted) "Permission granted" else "Permission NOT granted"
+            Log.i("MainActivity", message)
+        }
+
+    private val timerViewModel = TimerViewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CountdownTimerTheme(dynamicColor = false) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TimerScreen(timerViewModel = timerViewModel)
+                }
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+                permissionRequestLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        // Start TimerWorker if the timer is running
+        if (timerViewModel.isRunning) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.POST_NOTIFICATIONS) == zPackageManager.PERMISSION_GRANTED) {
+                    startWorker(timerViewModel.remainingMillis)
+                    timerViewModel.cancelTimer()
+                }
+            } else {
+                startWorker(timerViewModel.remainingMillis)
+                timerViewModel.cancelTimer()
+            }
+        }
+    }
+
+    private fun startWorker(millisRemain: Long) {
+        val timerWorkRequest: WorkRequest = OneTimeWorkRequestBuilder<TimerWorker>()
+            .setInputData(
+                workDataOf(
+                    KEY_MILLIS_REMAINING to millisRemain
+                )
+            ).build()
+
+        WorkManager.getInstance(applicationContext).enqueue(timerWorkRequest)
+    }
+
+
+}
---
+++ lab5/CountdownTimer/app/src/main/java/com/example/countdowntimer/ui/theme/TimerScreen.kt
@@ -0,0 +1,4 @@
+package com.example.countdowntimer.ui.theme
+
+class TimerScreen {
+}
